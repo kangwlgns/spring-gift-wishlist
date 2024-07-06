6cb6f5eaced1d2f183ee398c87a5ecf0028caa5d
kangwlgns <kangji0615@gmail.com>
2024-07-07 03:47:16 +0900

    refactor(AuthController): modify controller to RestController

--- src/main/java/gift/permissions/controller/AuthController.java
+++ src/main/java/gift/permissions/controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 // 로그인 또는 회원가입을 통해 이메일과 비밀번호를 받아서 토큰을 반환해주는 컨트롤러
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
 
@@ -22,15 +22,17 @@ public class AuthController {
 
     // 회원 가입에 대한 핸들러
     @PostMapping("/registration")
-    public TokenDto register(@RequestBody @Valid UserDto userDto) {
-        // service로부터 비즈니스 로직을 완수한 결과를 반환
-        return authService.register(userDto);
+    public void register(@RequestBody @Valid UserDto userDto) {
+        // service로부터 비즈니스 로직(회원가입)을 완료하고 끝.
+        // 보통 회원가입을 마치면 로그인 페이지로 돌려보내므로 이를 구현하는 데에 굳이 Token을 반환할 필요는 없다고 판단.
+        authService.register(userDto);
     }
 
     // 로그인에 대한 핸들러
     @PostMapping("/login")
-    public TokenDto login(@ModelAttribute @Valid UserDto userDto) {
-        // service로부터 비즈니스 로직을 완수한 결과를 반환
+    public TokenDto login(@RequestBody @Valid UserDto userDto) {
+        // service로부터 비즈니스 로직을 완수한 결과(토큰)를 반환.
+        // 토큰을 반환하는 역할만 하도록 함.
         return authService.login(userDto);
     }
 }
