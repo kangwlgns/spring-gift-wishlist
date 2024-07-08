5a5b7e71bacbebee12a685c812c24f2a89965b3e
kangwlgns <kangji0615@gmail.com>
2024-07-09 02:06:38 +0900

    refactor: update dtos

--- src/main/java/gift/product/dto/ProductUserRequestDto.java
+++ src/main/java/gift/product/dto/ProductUserRequestDto.java
@@ -1,6 +1,6 @@
 package gift.product.dto;
 
 // 유저로부터 입력 받는 dto. 이미 존재하는 제품에 대한 요청이므로 유효성 검증 x
-public record ProductUserRequestDto(String name, int price, String imageUrl) {
+public record ProductUserRequestDto(long id, String name, int price, String imageUrl) {
 
 }
--- src/main/java/gift/product/dto/ProductUserResponseDto.java
+++ src/main/java/gift/product/dto/ProductUserResponseDto.java
@@ -1,6 +1,6 @@
 package gift.product.dto;
 
-// 유저에게 보여즐 제품에 대한 dto (id는 보여줄 필요가 없음)
-public record ProductUserResponseDto(String name, int price, String imageUrl) {
+// 유저에게 보여즐 제품에 대한 dto. id는 보여주진 않지만 필요(구분자)
+public record ProductUserResponseDto(long id, String name, int price, String imageUrl) {
 
 }
