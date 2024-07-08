492025e6811d99e9650bd76bf799ce78060e7c77
kangwlgns <kangji0615@gmail.com>
2024-07-08 22:39:05 +0900

    refactor: rename packages

--- src/main/java/gift/permissions/component/LoginInterceptor.java
+++ src/main/java/gift/permissions/component/LoginInterceptor.java
@@ -4,6 +4,7 @@ import gift.permissions.component.TokenComponent;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -21,7 +22,7 @@ public class LoginInterceptor implements HandlerInterceptor {
     // controller 호출 전에 가로채는 previous interceptor
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        String[] token = request.getHeader("Authorization").split(" ");
+        String[] token = request.getHeader(HttpHeaders.AUTHORIZATION).split(" ");
         // token[0] == "Bearer". 인증 방식을 넣어줘야 한다고 들어서 이런 식으로 작성하였습니다.
         String tokenString = token[1];
         tokenComponent.validateToken(tokenString);
